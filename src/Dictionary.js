359183fef5d7449c332eb87df6474fd57302af75
aebailu <anna.baillie@outlook.com>
2025-06-23 12:06:33 +0100

    Added photos
    Added photos from API and styled

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,22 +2,30 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Definition from "./Definition.js";
+import Photos from "./Photos.js";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
   let [definition, setDefinition] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleKeyWordChange(event) {
     setKeyWord(event.target.value);
   }
   function handleResponse(response) {
-    console.log(response.data);
-    console.log(response.data.meanings[0].definition);
     setDefinition(response.data);
+  }
+
+  function handlePhotoResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
+    let photosApiKey = "8o2ff7498032b5ae4f9bt64daad694e7";
+    let photosApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyWord}&key=${photosApiKey}`;
+    axios.get(photosApiUrl).then(handlePhotoResponse);
     let apiKey = "8o2ff7498032b5ae4f9bt64daad694e7";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -37,6 +45,7 @@ export default function Dictionary() {
         </form>
       </div>
       <Definition definition={definition} />
+      <Photos photos={photos} />
     </div>
   );
 }
---
+++ src/Photos.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Photos(props) {
+  if (props.photos) {
+    console.log(props.photos);
+
+    return (
+      <div className="Photos row">
+        {props.photos.map(function (photo, index) {
+          return (
+            <div className="col-4" key={index}>
+              <a href={photo.url} target="_blank" rel="noreferrer">
+                <img class="img-fluid" src={photo.src.landscape} />
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
